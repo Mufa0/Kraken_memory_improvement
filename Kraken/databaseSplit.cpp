7bea5bf223887669ed7e0238bb0f5db38c20bdc9
Mufa0 <ms48948@fer.hr>
2017-05-31 13:23:56 +0200

    Added all Kraken code and created Makefile to compile

--- Kraken/databaseSplit.cpp
+++ Kraken/databaseSplit.cpp
@@ -1,22 +1,3 @@
-/*
- * Copyright 2013-2015, Derrick Wood <dwood@cs.jhu.edu>
- *
- * This file is part of the Kraken taxonomic sequence classification system.
- *
- * Kraken is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Kraken is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Kraken.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "kraken_headers.hpp"
 #include "krakendb.hpp"
 #include "krakenutil.hpp"
@@ -30,12 +11,8 @@ using namespace kraken;
 
 void parse_command_line(int argc, char **argv);
 void usage(int exit_code=EX_USAGE);
-void process_file(char *filename);
-void classify_sequence(DNASequence &dna, ostringstream &koss,
-                       ostringstream &coss, ostringstream &uoss);
-string hitlist_string(vector<uint32_t> &taxa, vector<uint8_t> &ambig);
-set<uint32_t> get_ancestry(uint32_t taxon);
-void report_stats(struct timeval time1, struct timeval time2);
+
+
 
 int Num_threads = 1;
 string DB_filename, Index_filename, Nodes_filename;
@@ -84,149 +61,15 @@ int main(int argc, char **argv) {
     idx_file.load_file();
   KrakenDBIndex db_index(idx_file.ptr());
   Database.set_index(&db_index);
-
-  /**if (Populate_memory)
-    cerr << "complete." << endl;
-
-  if (Print_classified) {
-    if (Classified_output_file == "-")
-      Classified_output = &cout;
-    else
-      Classified_output = new ofstream(Classified_output_file.c_str());
-  }
-
-  if (Print_unclassified) {
-    if (Unclassified_output_file == "-")
-      Unclassified_output = &cout;
-    else
-      Unclassified_output = new ofstream(Unclassified_output_file.c_str());
-  }
-
-  if (! Kraken_output_file.empty()) {
-    if (Kraken_output_file == "-")
-      Print_kraken = false;
-    else
-      Kraken_output = new ofstream(Kraken_output_file.c_str());
-  }
-  else
-    Kraken_output = &cout;
-
-  struct timeval tv1, tv2;
-  gettimeofday(&tv1, NULL);
-  for (int i = optind; i < argc; i++)
-    process_file(argv[i]);
-  gettimeofday(&tv2, NULL);
-  **/
-  report_stats(tv1, tv2);
-
   return 0;
 }
 void parse_command_line(int argc, char **argv) {
   int opt;
   long long sig;
 
-  if (argc > 1 && strcmp(argv[1], "-h") == 0)
-    usage(0);
-  while ((opt = getopt(argc, argv, "d:i:t:u:n:m:o:qfcC:U:M")) != -1) {
-    switch (opt) {
-      case 'd' :
-        DB_filename = optarg;
-        break;
-      case 'i' :
-        Index_filename = optarg;
-        break;
-      case 't' :
-        sig = atoll(optarg);
-        if (sig <= 0)
-          errx(EX_USAGE, "can't use nonpositive thread count");
-        #ifdef _OPENMP
-        if (sig > omp_get_num_procs())
-          errx(EX_USAGE, "thread count exceeds number of processors");
-        Num_threads = sig;
-        omp_set_num_threads(Num_threads);
-        #endif
-        break;
-      case 'n' :
-        Nodes_filename = optarg;
-        break;
-      case 'q' :
-        Quick_mode = true;
-        break;
-      case 'm' :
-        sig = atoll(optarg);
-        if (sig <= 0)
-          errx(EX_USAGE, "can't use nonpositive minimum hit count");
-        Minimum_hit_count = sig;
-        break;
-      case 'f' :
-        Fastq_input = true;
-        break;
-      case 'c' :
-        Only_classified_kraken_output = true;
-        break;
-      case 'C' :
-        Print_classified = true;
-        Classified_output_file = optarg;
-        break;
-      case 'U' :
-        Print_unclassified = true;
-        Unclassified_output_file = optarg;
-        break;
-      case 'o' :
-        Kraken_output_file = optarg;
-        break;
-      case 'u' :
-        sig = atoll(optarg);
-        if (sig <= 0)
-          errx(EX_USAGE, "can't use nonpositive work unit size");
-        Work_unit_size = sig;
-        break;
-      case 'M' :
-        Populate_memory = true;
-        break;
-      default:
-        usage();
-        break;
-    }
-  }
-
-  if (DB_filename.empty()) {
-    cerr << "Missing mandatory option -d" << endl;
-    usage();
-  }
-  if (Index_filename.empty()) {
-    cerr << "Missing mandatory option -i" << endl;
-    usage();
-  }
-  if (Nodes_filename.empty() && ! Quick_mode) {
-    cerr << "Must specify one of -q or -n" << endl;
-    usage();
-  }
-  if (optind == argc) {
-    cerr << "No sequence data files specified" << endl;
-  }
 }
 
 void usage(int exit_code) {
-  cerr << "Usage: classify [options] <fasta/fastq file(s)>" << endl
-       << endl
-       << "Options: (*mandatory)" << endl
-       << "* -d filename      Kraken DB filename" << endl
-       << "* -i filename      Kraken DB index filename" << endl
-       << "  -n filename      NCBI Taxonomy nodes file" << endl
-       << "  -o filename      Output file for Kraken output" << endl
-       << "  -t #             Number of threads" << endl
-       << "  -u #             Thread work unit size (in bp)" << endl
-       << "  -q               Quick operation" << endl
-       << "  -m #             Minimum hit count (ignored w/o -q)" << endl
-       << "  -C filename      Print classified sequences" << endl
-       << "  -U filename      Print unclassified sequences" << endl
-       << "  -f               Input is in FASTQ format" << endl
-       << "  -c               Only include classified reads in output" << endl
-       << "  -M               Preload database files" << endl
-       << "  -h               Print this message" << endl
-       << endl
-       << "At least one FASTA or FASTQ file must be specified." << endl
-       << "Kraken output is to standard output by default." << endl;
+
   exit(exit_code);
 }
